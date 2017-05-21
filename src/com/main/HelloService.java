933c74bed6037e7b233351864cbd697e8334502d
Sandeep Bamane <sandeep.bamane@amdocs.com>
2017-05-21 06:32:39 +0530

    making XML response working for http://localhost:8080/vogellaRestImpl/rest/hello

--- src/com/main/HelloService.java
+++ src/com/main/HelloService.java
@@ -5,14 +5,26 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.main.models.UserInfo;
+
 //Sets the path to base URL + /hello
 @Path("/hello")
 public class HelloService {
 	
-	@GET
+	/*@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	 public String sayPlainTextHello() {
 	    return "Hello Jersey";
+	  }
+	  */
+	
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	 public UserInfo sayPlainTextHello() {
+	    UserInfo userInfo = new UserInfo();
+	    userInfo.setId(1);
+	    userInfo.setName("SANDEEP");
+	    return userInfo;
 	  }
 
 
